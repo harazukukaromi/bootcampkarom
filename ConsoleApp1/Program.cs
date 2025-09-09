3cbeae8367239fc94e244734e79b721e316baeb6
alfikarom <alfikarom12@gmail.com>
2025-09-09 14:57:04 +0700

    Your commit message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,24 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("input integer n: ");
-int n = int.Parse(Console.ReadLine());
-
-for (int i = 1; i <= n; i++)
+using System;
+class ConsoleApp1
 {
-    if (i % 3 == 0 && i % 5 == 0)
+    static void Main()
     {
-        Console.Write("foobar,");
-    }
-    else if (i % 3 == 0)
-    {
-        Console.Write("foo,");
-    }
-    else if (i % 5 == 0)
-    {
-        Console.Write("bar,");
-    }
-        else
-    {
-        Console.Write(i + ",");
+        Console.Write("input integer n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.Write("foobar,");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.Write("foo,");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("bar,");
+            }
+            else
+            {
+                Console.Write(i + ",");
+            }
+        }
     }
 }
 
