6dedb4704d6eb087b20681edc97bb0ec6e5f000c
alfikarom <alfikarom12@gmail.com>
2025-09-10 09:28:32 +0700

    Your commit message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,55 +5,17 @@ class ConsoleApp1
 {
     static void Main()
     {
-        //Perhitungan dua bilangan integer
-        Console.Write("Masukan Nilai integer: ");
-        int x = int.Parse(Console.ReadLine());
-
-        Console.Write("Masukan Nilai Integer: ");
-        int y = int.Parse(Console.ReadLine());
-
-        int z = x * y;
-        Console.WriteLine("hasil perkalian: " + z);
-
-        //Perhitungan dua bilangan decimal
-        Console.Write("masukan Nilai decimal pertama: ");
-        double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        Console.Write("masukan Nilai decimal kedua: ");
-        double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        double c = a + b;
-        Console.WriteLine("Hasil pemjumlahan: " + c);
-
-        //pengurangan dua bilangan decimal
-        Console.Write("masukan Nilai decimal pertama: ");
-        double i = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        Console.Write("masukan Nilai decimal kedua: ");
-        double j = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        double k = i - j;
-        Console.WriteLine("Hasil pengurangan: " + k);
-
-        //pembagian dua bilangan decimal
-        Console.Write("masukan Nilai decimal pertama: ");
-        double m = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        Console.Write("masukan Nilai decimal kedua: ");
-        double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-        double o = m / n;
-        Console.WriteLine("Hasil pembagian: " + o);
-
-        //modulus dua bilangan integer
-        Console.Write("Masukan Nilai integer: ");
-        int s = int.Parse(Console.ReadLine());
-
-        Console.Write("Masukan Nilai Integer: ");
-        int t = int.Parse(Console.ReadLine());
-
-        int u = s % t;
-        Console.WriteLine("hasil modulus: " + u);
+        Console.Write("Masukan Nilai integer atau Decimal: ");
+        double input = Convert.ToDouble(Console.ReadLine());
+        
+        if (input % 1 == 0)
+        {
+            Console.WriteLine("Merupakan nilai integer: " + Convert.ToInt32(input));
+        }
+        else
+        {
+            Console.WriteLine("Merupakan nilai decimal: " + input.ToString("F2", CultureInfo.InvariantCulture));
+        }
     }
 }
 
