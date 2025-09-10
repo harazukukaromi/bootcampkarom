3a4a7095741c9d38d39496eb1c461972afc839b7
alfikarom <alfikarom12@gmail.com>
2025-09-10 10:01:31 +0700

    your commit message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,21 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Globalization;
+using System.Text.RegularExpressions;
+
 class ConsoleApp1
 {
     static void Main()
     {
-        // Boolean Equality
-        Console.WriteLine("Input nilai x dalam integer:");
-        int x = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.WriteLine("Input nilai y dalam integer:");
-        int y = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.WriteLine("Input nilai z dalam integer:");
-        int z = Convert.ToInt32(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.WriteLine($"{x} == {y} : {x == y}");
-        Console.WriteLine($"{x} == {z} : {x == z}");
-        Console.WriteLine($"{x} != {y} : {x != y}");
-        Console.WriteLine($"{x} != {z} : {x != z}");
+        //string kalimat
+        Console.Write("input kalimat: ");
+        string kalimat = Console.ReadLine();
+
+        string HasilAwal = kalimat.ToLower();
+        HasilAwal = Regex.Replace(HasilAwal, @"[^\w\s]", "");
+        HasilAwal = Regex.Replace(HasilAwal, @"\s+", "");
+        HasilAwal = Regex.Replace(HasilAwal, @"\d", "");
+        Console.WriteLine("Kalimat akhir: " + HasilAwal);
     }
 }
 
