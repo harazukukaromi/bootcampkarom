b3cd63a7c1dfe00d3194d5cba75c63fa92bfaac1
alfikarom <alfikarom12@gmail.com>
2025-09-10 13:11:12 +0700

    your commit message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,44 +6,27 @@ class ConsoleApp1
 {
     static void Main()
     {
-        // tenary operator
-        int a = 40;
-        int b = 60;
-        int max = (a > b) ? a : b;
-        Console.WriteLine("Nilai maksimum adalah: " + max);
+        //null operators
+        string? nama = null;
+        Console.WriteLine(nama ?? "nama tidak diisi");
+        nama = "Hikaromi";
+        Console.WriteLine(nama ?? "nama tidak diisi");
 
-        //primary expression
-        int n = 5;
-        for (int i = 1; i <= n; i++)
-        {
-            Console.Write(i); // Tambahkan titik koma di akhir baris ini
-            if (i < n)
-            {
-                Console.Write(", ");
-            }
-        }
-        Console.WriteLine();
-        //void expression
-        void PrintMessage(string message)
-        {
-            Console.WriteLine(message);
-        }
-        PrintMessage("Hello, World!");
-        //assignment expression
-        int x;
-        x = 42;
-        Console.WriteLine("Nilai x adalah: " + x);
-        //precendence expression
-        int result = 10 + 5 * 2; // Perkalian (*) memiliki
-        // presedensi lebih tinggi daripada penjumlahan (+)
-        Console.WriteLine("Hasilnya adalah: " + result); // Output: 20
-        //associativity expression
-        int value = 100 / 5 / 2; // Pembagian (/) memiliki
-        // asosiativitas kiri ke kanan
-        Console.WriteLine("Nilai akhirnya adalah: " + value);// Output: 10
-        //right to left associativity
-        int y = 10;
-        y += 5; // Sama dengan y = y + 5
-        Console.WriteLine("Nilai y adalah: " + y); // Output: 15
+        string s1 = null;
+        string s2 = s1 ?? "s1 is null";
+        Console.WriteLine(s2);
+
+        //null-coalescing assignment operator
+        string? s3 = null;
+        s3 ??= "s3 is null";
+        Console.WriteLine(s3);
+
+        //null-conditional operator
+        string? s4 = null;
+        int? length = s4?.Length;
+        Console.WriteLine(length ?? 0);
+        s4 = "Hikaromi";
+        length = s4?.Length;
+        Console.WriteLine(length ?? 0);
     }
 }
