61995b1051bb55f715ff9a33d568fbb931b83b3f
alfikarom <alfikarom12@gmail.com>
2025-09-10 10:10:13 +0700

    your message commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,10 +12,12 @@ class ConsoleApp1
         string kalimat = Console.ReadLine();
 
         string HasilAwal = kalimat.ToLower();
-        HasilAwal = Regex.Replace(HasilAwal, @"[^\w\s]", ""); // menghilangkan tanda baca
-        HasilAwal = Regex.Replace(HasilAwal, @"\s+", ""); // menghilangkan spasi
+        //HasilAwal = Regex.Replace(HasilAwal, @"[^\w\s]", ""); // menghilangkan tanda baca
+        //HasilAwal = Regex.Replace(HasilAwal, @"\s+", ""); // menghilangkan spasi
         //HasilAwal = Regex.Replace(HasilAwal, @"\d", ""); // menghilangkan angka
-        HasilAwal = Regex.Replace(HasilAwal, @"[a-zA-Z]", ""); // menghilangkan huruf
+        //HasilAwal = Regex.Replace(HasilAwal, @"[a-zA-Z]", ""); // menghilangkan huruf
+        //HasilAwal = Regex.Replace(HasilAwal, @"[aAiIuUeEoO]", ""); // menghilangkan huruf vokal
+        HasilAwal = Regex.Replace(HasilAwal, @"[BbRrIi]", ""); // menghilangkan huruf tertentu dalam konteks b, r, dan i
         Console.WriteLine("Kalimat akhir: " + HasilAwal);
     }
 }
