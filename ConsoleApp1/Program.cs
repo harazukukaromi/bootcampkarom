7d05f2ee750ef89c436cc01750ced5e9c81ad228
alfikarom <alfikarom12@gmail.com>
2025-09-10 10:06:49 +0700

    your commit message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,14 +7,15 @@ class ConsoleApp1
 {
     static void Main()
     {
-        //string kalimat
+        //string kalimat menghilangkan kondisi tidak perlukan
         Console.Write("input kalimat: ");
         string kalimat = Console.ReadLine();
 
         string HasilAwal = kalimat.ToLower();
-        HasilAwal = Regex.Replace(HasilAwal, @"[^\w\s]", "");
-        HasilAwal = Regex.Replace(HasilAwal, @"\s+", "");
-        HasilAwal = Regex.Replace(HasilAwal, @"\d", "");
+        HasilAwal = Regex.Replace(HasilAwal, @"[^\w\s]", ""); // menghilangkan tanda baca
+        HasilAwal = Regex.Replace(HasilAwal, @"\s+", ""); // menghilangkan spasi
+        //HasilAwal = Regex.Replace(HasilAwal, @"\d", ""); // menghilangkan angka
+        HasilAwal = Regex.Replace(HasilAwal, @"[a-zA-Z]", ""); // menghilangkan huruf
         Console.WriteLine("Kalimat akhir: " + HasilAwal);
     }
 }
