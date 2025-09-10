233a691cb043c0e597f1ac8c022142c2dfecda34
alfikarom <alfikarom12@gmail.com>
2025-09-10 09:38:29 +0700

    Your commit message

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,17 +5,25 @@ class ConsoleApp1
 {
     static void Main()
     {
-        Console.Write("Masukan Nilai integer atau Decimal: ");
-        double input = Convert.ToDouble(Console.ReadLine());
-        
-        if (input % 1 == 0)
-        {
-            Console.WriteLine("Merupakan nilai integer: " + Convert.ToInt32(input));
-        }
-        else
-        {
-            Console.WriteLine("Merupakan nilai decimal: " + input.ToString("F2", CultureInfo.InvariantCulture));
-        }
+        // Oprational boolean &&, ||, !
+        bool a = false;
+        bool b = false;
+        bool c = true;
+        bool d = true;
+        Console.WriteLine(a && b); 
+        Console.WriteLine(a && c); 
+        Console.WriteLine(c && d); 
+        Console.WriteLine(a || c); 
+        Console.WriteLine(a || b);
+        Console.WriteLine(c || d); 
+        Console.WriteLine(!a); 
+        Console.WriteLine(!c); 
+        Console.WriteLine(!a && c || d);
+        Console.WriteLine(!(a && c) || d); 
+        Console.WriteLine(!(a && c || d)); 
+        Console.WriteLine(!a && (c || d));
+        Console.WriteLine(!a && c || !d); 
+        Console.WriteLine(!a && (c || !d));
     }
 }
 
