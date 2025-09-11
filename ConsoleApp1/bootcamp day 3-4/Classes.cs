c51469a7294a0da67cdd58cd7f1ed4838d29e1d9
alfikarom <alfikarom12@gmail.com>
2025-09-11 14:14:44 +0700

    your message commit

--- ConsoleApp1/bootcamp day 3-4/Classes.cs
+++ ConsoleApp1/bootcamp day 3-4/Classes.cs
@@ -1,5 +1,5 @@
 // See https://aka.ms/new-console-template for more information
-using System;
+/*using System;
 
 class Classes
 {
@@ -43,6 +43,5 @@ class Classes
 
         // Local method to calculate volume of a tube
         double VolumeTube(double radius,double height) => Math.PI * radius * radius * height;
-
     }
-}
+}*/
---
+++ ConsoleApp1/bootcamp day 3-4/Classes2.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Classes2
+{
+    //instance Constructor
+    static void Main(string[] args)
+    {
+        Panda p1 = new Panda("Kung Fu Panda");
+        Panda p2 = new Panda("Poo");
+
+        p1.Introduce();
+        p2.Introduce();
+    }
+}
+
+public class Panda
+{
+    private string name; // Instance field
+
+    // Instance constructor
+    public Panda(string name)
+    {
+        this.name = name;
+        Console.WriteLine("Panda instance created!");
+    }
+
+    // Instance method
+    public void Introduce()
+    {
+        Console.WriteLine($"Hi, I'm a panda named {name}.");
+    }
+}
+
