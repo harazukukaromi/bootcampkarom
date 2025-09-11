ff7f648fc5bac2125de16ac4f35fe8de6a31652a
alfikarom <alfikarom12@gmail.com>
2025-09-11 09:31:41 +0700

    your message commit

---
+++ ConsoleApp1/Classes.cs
@@ -0,0 +1,33 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+
+class ConsoleApp
+{
+    static void Main(string[] args)
+    {
+        //create an instance of Person
+        Person person = new Person("Hikaromi", 130);
+        //call the Introduce method
+        person.Introduce();
+    }
+    //classes
+    class Person
+    {
+        //fields
+        private string name;
+        private float height;
+
+        //constructor
+        public Person(string name, float height)
+        {
+            this.name = name;
+            this.height = height;
+        }
+
+        //methods
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {name} and I am {height} cm.");
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Text.RegularExpressions;
 
 class ConsoleApp1
 {
-    static void Main()
-    {
-        //break statement
-        int n = 10;
-        for (int i = 1; i <= n; i++)
-        {
-            if (i == 5)
-            {
-                break; // Keluar dari loop ketika i sama dengan 5
-            }
-            Console.Write(i + " ");
-        }
-        //continue statement
-        Console.WriteLine();
-        for (int i = 1; i <= n; i++)
-        {
-            if (i == 5)
-            {
-                continue; // Lewati iterasi ketika i sama dengan 5
-            }
-            Console.Write(i + " ");
-        }
-        //goto statement
-        int i = 1;
-        startLoop: // This is a label
-        if (i <= 5)
-        {
-            Console.Write(i + " ");
-            i++;
-            goto startLoop; // Jumps back to the 'startLoop' label
-        }
-    }
 }
