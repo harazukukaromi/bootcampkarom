5d9bfa91f9e37310964dadf760d7ef77382a28cf
alfikarom <alfikarom12@gmail.com>
2025-10-07 09:29:38 +0700

    your message commit

--- ProductApi/obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1a219eab5c9b895ed213d14cbe7c5016fbee73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb459384db4eb714b811aa4bb23672b17a7a1506")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
