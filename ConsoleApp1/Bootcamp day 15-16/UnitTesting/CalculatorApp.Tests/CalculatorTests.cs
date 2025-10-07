380ab10a16fe39f7cedd9b19d0357bc5e4363690
alfikarom <alfikarom12@gmail.com>
2025-10-07 10:33:42 +0700

    your message commit

--- ConsoleApp1/Bootcamp day 15-16/UnitTesting/CalculatorApp.Tests/CalculatorTests.cs
+++ ConsoleApp1/Bootcamp day 15-16/UnitTesting/CalculatorApp.Tests/CalculatorTests.cs
@@ -1,10 +1,25 @@
-﻿namespace CalculatorApp.Tests;
+﻿using Xunit;
+using CalculatorApp;
 
-public class UnitTest1
+namespace CalculatorApp.Tests
 {
-    [Fact]
-    public void Test1()
+    public class CalculatorTests
     {
+        [Fact]
+        public void Add_ShouldReturnSum_WhenGivenTwoNumbers()
+        {
+            var calc = new Calculator();
+            int result = calc.Add(3, 5);
+            Assert.Equal(8, result);
+        }
 
+        [Fact]
+        public void Subtract_ShouldReturnDifference_WhenGivenTwoNumbers()
+        {
+            var calc = new Calculator();
+            int result = calc.Subtract(10, 4);
+            Assert.Equal(6, result);
+        }
     }
 }
+
