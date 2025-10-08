6e30bf466d5252e4abbe8b9c25d2be657f661c03
alfikarom <alfikarom12@gmail.com>
2025-10-08 10:11:56 +0700

    your message commit

--- RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net9.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9f5d04d4dc398fb1f47ca4301346d22750ce70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95345dd5d4dd614b42692af01567334647ac1274")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
